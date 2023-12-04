0c4445980ddab773cc603b08589452f2484e2c3f
Evalyne306 <wanjikueva306@gmail.com>
2023-12-04 22:39:24 +0300

    family tree update

--- src/FamilyTree.js
+++ src/FamilyTree.js
@@ -1,28 +1,48 @@
-import React from "react";
-import video2 from '/home/student/Begin/truestory/src/images/family.png';
-import './FamilyTree.css'
+// import React from "react";
+// import video2 from '/home/student/Begin/truestory/src/images/family.png';
+// import './FamilyTree.css'
 
 
 
 
-function FamilyTree() {
-  return (
-    <div className="family-page" id="family-tree">
-      <div>
-      <video autoPlay muted loop id="background-video">
-        <source src={video2} />
-      </video>
-      </div>
+// function FamilyTree() {
+//   return (
+//     <div className="family-page" id="family-tree">
+//       <div>
+//       <video autoPlay muted loop id="background-video">
+//         <source src={video2} />
+//       </video>
+//       </div>
      
 
-      <div className="overlay">
+//       <div className="overlay">
      
    
      
-      </div>
+//       </div>
      
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
+// export default FamilyTree;
+import React from "react";
+// import picture from '/home/student/true-story-app/src/images/phone.png';
+import picture1 from '/home/student/Begin/truestory/src/images/family.png';
+import './ContactUs.css';
+
+
+function FamilyTree(){
+
+return(
+
+<div className="family-page" id="family-tree">
+<img src={picture1} alt="placeholder" />
+
+</div>
+
+
+);
+
+}
 export default FamilyTree;
