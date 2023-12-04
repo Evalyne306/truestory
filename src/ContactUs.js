f975cc1c681d7754c87695bb2c229d97176e9a95
Evalyne306 <wanjikueva306@gmail.com>
2023-12-05 01:14:55 +0300

    updates

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import picture from '/home/student/true-story-app/src/images/phone.png';
-import picture1 from '/home/student/Begin/truestory/src/images/cont.png';
+import picture1 from '/home/student/Begin/truestory/src/images/touch2.png';
 import './ContactUs.css';
 
 
